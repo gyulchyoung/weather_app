7ee2da785ac6b2b896bffb0054e9784d2edb39c9
gyulchyoung <hacha2011@naver.com>
2021-03-21 21:26:19 +0900

    edit design, weekly

--- app/src/main/java/com/example/Magic_CnyangE/weekly/WeeklyFragment.java
+++ app/src/main/java/com/example/Magic_CnyangE/weekly/WeeklyFragment.java
@@ -105,7 +105,7 @@ public class WeeklyFragment extends Fragment {
         adapter = new WeeklyMainAdapter();
         recyclerView.setAdapter(adapter);
 
-        initData();
+
 
         String tempCode = PreferenceManager.getString(getContext(), "locationCityCode");
         String weatherCode = PreferenceManager.getString(getContext(), "locationCountryCode");
@@ -116,6 +116,7 @@ public class WeeklyFragment extends Fragment {
             tempCode = "11B10101";
             weatherCode = "11B00000";
         }
+        initData();
 
         Call<WeeklyModel_weather> getWeatherInstance = weeklyRetrofitFactory_weather.getWeekly_weatherApi()
                 .getList(API_KEY, "1", weatherCode, date+set_time, "JSON");//하루전꺼 조사해서 3,4,5,6이면 당일기준 2,3,4,5 시간수정
